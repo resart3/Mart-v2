bb9a513a45e289b4ba5028c40c4b837b841b88a0
ramadhangandidi <thinkpad1st@gmail.com>
2022-08-05 22:21:19 +0700

    Update get transaction for mobile

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -7,6 +7,7 @@ use Api;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Response;
 use File;
+use Illuminate\Support\Facades\DB;
 
 class TransactionController extends Controller
 {
@@ -20,19 +21,72 @@ class TransactionController extends Controller
     public function index()
     {
         try {
-            $response = Transaction::where('family_card_id', (new Transaction)->get_family_card());
-
+            $response = Transaction::where('family_card_id', (new Transaction)->get_family_card());        
             if(request()->has('tahun')){
                 $response = $response->where('tahun', request()->query('tahun'));
             }
+            $this->response = Api::pagination($response);            
 
-            $this->response = Api::pagination($response);
+            $dataLand = DB::table('lands')
+            ->join('categories', 'categories.id', '=', 'lands.category_id')
+            ->where('family_card_id', (new Transaction)->get_family_card())
+            ->select('categories.amount')        
+            ->get();
+
+            $jumlahIuran = 0;
+            foreach($dataLand as $data){
+                $jumlahIuran = $jumlahIuran + $data->amount;
+            }
+            
+            $arrBulan = ["Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", 
+                "September", "Oktober", "November", "Desember"];
+            $arrDataBulan = [];
+            $arrDataTransaksi = [];
+
+            foreach($this->response as $data){                
+                array_push($arrDataBulan, $data->bulan);
+                $tahun = $data->tahun;
+            }            
+            
+            foreach($arrDataBulan as $dataBulan){                
+                foreach($arrBulan as $bulan){                    
+                    if($dataBulan == $bulan){
+                        if (($key = array_search($bulan, $arrBulan)) !== false) {
+                            unset($arrBulan[$key]);
+                        }
+                    }                    
+                }
+            }
+
+            if(isset($tahun)){                
+                foreach($arrBulan as $data){
+                    $tempData["family_card_id"] = (new Transaction)->get_family_card();
+                    $tempData["jumlah"] = $jumlahIuran;
+                    $tempData["tahun"] = $tahun;
+                    $tempData["bulan"] = $data;
+                    $tempData["status"] = "Belum Membayar";
+                    array_push($arrDataTransaksi, $tempData);
+                }
+    
+                foreach($arrDataBulan as $data){
+                    $tempData["family_card_id"] = (new Transaction)->get_family_card();
+                    $tempData["jumlah"] = $jumlahIuran;
+                    $tempData["tahun"] = $tahun;
+                    $tempData["bulan"] = $data;
+                    $tempData["status"] = "Lunas";
+                    array_push($arrDataTransaksi, $tempData);
+                }
+            }else{
+                $arrDataTransaksi = [];
+            }
+            
+
         } catch (Exception $e){
             $this->code = 500;
             $this->response = $e->getMessage();
         }
 
-        return Api::apiRespond($this->code, $this->response);
+        return Api::apiRespond($this->code, $arrDataTransaksi);
     }
 
     public function show($id)
